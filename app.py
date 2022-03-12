8a8b2d6a0d8e6e0f670aff3388b00986a076b875
farukiramly <you@example.com>
2022-03-12 13:13:52 +0800

    commit

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ st.title("Welcome to House Prediction app")
 st.image(image, use_column_width=True)
 
 #---SIDEBAR
-col1, col2 = st.sidebar.columns(2)
+st.sidebar.columns(2)
 st.sidebar.title("🎊 Hello! Welcome 🎊")
 image = Image.open('profile.jpg')
-col1.image(image, use_column_width=True)
+image(image, use_column_width=True)
 # col2.write('Made with 💖 by')
 # col2.write('[*Faruki Ramly*](https://www.linkedin.com/in/farukiramly/)')
 
